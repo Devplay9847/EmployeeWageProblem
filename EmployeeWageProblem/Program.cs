f8035ae94b9238aa9cce3a0a334ccf04e55af70e
Devplay9847 <devendra.nishad.9847@gmail.com>
2022-05-01 12:43:47 +0530

    Welcome to Employee Wage problem Case UC1 ,UC2,UC3

---
+++ EmployeeWageProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage problem Case UC1 ,UC2,UC3");
+            Console.ReadLine();
+        }
+    }
+}
